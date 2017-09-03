78941aae1a60ed804c460e0e4335978645080bad
Louis Liao <louisliao@kkstream.com.tw>
2017-09-03 20:58:48 +0800

    Implement Node exercise (Node/graders.js)

--- Javascript/Node/grader.js
+++ Javascript/Node/grader.js
@@ -1,3 +1,15 @@
-function average(){
-	
+//Average Grade from couple grades
+
+function average(array){
+  let sum = 0
+	array.forEach(function(grade){
+  sum+=grade
+  })
+  return Math.round((sum/(array.length)))
 }
+
+
+const scores = [90, 98, 89, 100, 100, 86, 94]
+console.log(average(scores))
+const scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49]
+console.log(average(scores2))
