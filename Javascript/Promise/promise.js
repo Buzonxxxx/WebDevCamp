8a073767124856ee1ef405ad2d55945ab14781d8
Louis Liao <louisliao@kkstream.com.tw>
2017-09-09 22:41:11 +0800

    add jQuery

---
+++ Javascript/Promise/promise.js
@@ -0,0 +1,18 @@
+/*
+  states of promises
+  1. unresolved
+  2. resolved -> then
+  3. rejected -> catch
+*/
+
+promise = new Promise((resolve, reject) => {
+  // resolve()
+  setTimeout(() => {
+    resolve()
+  }, 3000)
+})
+
+promise
+.then(() => console.log('finally finished'))
+.then(() => console.log('I was also ran !!!'))
+.catch(() => console.log('uh ah!!'))
