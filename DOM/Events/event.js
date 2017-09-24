4a21fefe7738ed8a6a662f7a218505f38f108b6e
Louis Liao <louisliao@kkstream.com.tw>
2017-09-24 23:04:04 +0800

    Init DOM events

---
+++ DOM/Events/event.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button")
+const paragraph = document.querySelector("p")
+button.addEventListener("click", () => paragraph.textContent = "Someone Clicked The Button!"
+)
+
