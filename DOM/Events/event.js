08aa5ca4135d3a4a4788a35459512c84d0fcd5a0
Louis Liao <louisliao@kkstream.com.tw>
2017-09-25 14:50:22 +0800

    Fix event.js

--- DOM/Events/event.js
+++ DOM/Events/event.js
@@ -1,5 +1,4 @@
 const button = document.querySelector("button")
 const paragraph = document.querySelector("p")
-button.addEventListener("click", () => paragraph.textContent = "Someone Clicked The Button!"
-)
+button.addEventListener("click", () => paragraph.textContent = "Someone Clicked The Button!")
 
--- DOM/select_manipulate/script.js
+++ DOM/select_manipulate/script.js
@@ -1,5 +1,8 @@
 const h1 =  document.querySelector("h1");
 h1.style.color = "pink";
+h1.addEventListener("click", function(){
+  h1.textContent = "You clicked the h1!"
+})
 
 const body = document.querySelector("body");
 let isBlue = false;
