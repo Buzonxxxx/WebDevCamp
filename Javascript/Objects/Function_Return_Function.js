9ded7180e42322ae6a598a85d8db92ecac16a5a9
Louis Liao <louisliao@kkstream.com.tw>
2017-10-08 11:29:05 +0800

    Implement bind/call/apply function

--- Javascript/Objects/Function_Return_Function.js
+++ Javascript/Objects/Function_Return_Function.js
@@ -1,17 +1,17 @@
 // Functions returning functions #ToPractice
 
-function interviewQuestion(job){
-  if (job === 'designer'){
-    return function(name){
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function (name) {
       console.log(name + ', can you please explain what UX design is? ')
     }
-  }else if (job === 'teacher'){
-    return function(name){
-      console.log('What subject do you teach, '+ name + '?')
+  } else if (job === 'teacher') {
+    return function (name) {
+      console.log('What subject do you teach, ' + name + '?')
     }
-  }else {
-    return function(name){
-      console.log('Hello, '+name+', what do you do?')
+  } else {
+    return function (name) {
+      console.log('Hello, ' + name + ', what do you do?')
     }
   }
 }
