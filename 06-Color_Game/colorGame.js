57e548056bb4e7e73ecc4929b5ea318fb462edb3
Louis Liao <buzonliao@gmail.com>
2018-02-17 16:54:23 +0800

    Color Game: refactor

--- 06-Color_Game/colorGame.js
+++ 06-Color_Game/colorGame.js
@@ -6,57 +6,49 @@ const colorDisplay = document.querySelector("#colorDisplay")
 const messageDisplay = document.querySelector("#message")
 const h1 = document.querySelector("h1")
 const resetButton = document.querySelector("#reset")
-const easyBtn = document.querySelector("#easyBtn")
-const hardBtn = document.querySelector("#hardBtn")
+// const easyBtn = document.querySelector("#easyBtn")
+// const hardBtn = document.querySelector("#hardBtn")
+const modeButtons = document.querySelectorAll(".mode")
 
-colorDisplay.textContent = pickedColor
 
-easyBtn.addEventListener("click", () => {
-  hardBtn.classList.remove("selected")
-  easyBtn.classList.add("selected")
-  numSquares = 3
-  colors = generateRandomColors(numSquares)
-  pickedColor = pickColor()
-  colorDisplay.textContent = pickedColor
-  squares.forEach(
-    (square, i) => {
-      if (colors[i]) {
-        square.style.backgroundColor = colors[i]
-      } else {
-        square.style.display = "none"
-      }
-    })
+modeButtons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    modeButtons[0].classList.remove("selected")
+    modeButtons[1].classList.remove("selected")
+    button.classList.add("selected")
+    button.textContent === "Easy" ? numSquares = 3 : numSquares = 6
+    reset()
+
+  })
 })
 
-hardBtn.addEventListener("click", () => {
-  easyBtn.classList.remove("selected")
-  hardBtn.classList.add("selected")
-  numSquares = 6
-  colors = generateRandomColors(numSquares)
-  pickedColor = pickColor()
-  colorDisplay.textContent = pickedColor
-  squares.forEach(
-    (square, i) => {
-      square.style.backgroundColor = colors[i]
-      square.style.display = "block"
-    })
-
-})
-
-resetButton.addEventListener("click", () => {
+function reset() {
   //generate all new colors
   colors = generateRandomColors(numSquares)
   //pick a new random color from array
   pickedColor = pickColor()
   //change colorDisplay to match picked color
   colorDisplay.textContent = pickedColor
+  resetButton.textContent = "New Colors"
+  messageDisplay.textContent = ""
   //change colors of squares
   squares.forEach(
     (square, i) => {
-      square.style.backgroundColor = colors[i]
+      if (colors[i]) {
+        square.style.display = "block"
+        square.style.backgroundColor = colors[i]
+      } else {
+        square.style.display = "none"
+      }
     })
   h1.style.backgroundColor = "steelblue"
+}
+
+resetButton.addEventListener("click", () => {
+  reset()
 })
+
+colorDisplay.textContent = pickedColor
 
 squares.forEach(
   (square, i) => {
