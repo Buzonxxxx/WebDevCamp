f71860d5f7251f86c92e4943b1aba7be1cc5087b
Louis Liao <buzonliao@gmail.com>
2018-06-12 21:35:06 +0800

    add web server

---
+++ 08-NodeJS/node-web-server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>')
+  res.send({
+    name: 'Louis',
+    likes: [
+      'Biking',
+      'Running',
+      'Swimming'
+    ]
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request!'
+  })
+})
+app.listen(3000, () => {
+  console.log('server is up on port 3000')
+})
