b7d0cfdc426dc85c501af043e15c71c960502bdb
Louis Liao <buzonliao@gmail.com>
2018-08-20 17:59:48 +0800

    finish JQuery Todo List

--- 07-JQuery/ToDoApp/assets/js/todos.js
+++ 07-JQuery/ToDoApp/assets/js/todos.js
@@ -1,1 +1,22 @@
-alert('CONNECTED')
+
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass('completed')
+})
+
+$('ul').on('click', 'span', function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove()
+  })
+  event.stopPropagation()
+})
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){ // 13 is enter key
+    let todoText = $(this).val()
+    $('ul').append(`<li><span><i class="fa fa-trash"></i></span> ${todoText}</li>`)
+  }
+})
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
