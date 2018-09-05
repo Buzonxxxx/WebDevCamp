0b765ae00fcdce7d7d62c7f895c5113730d0be4b
Louis Liao <buzonliao@gmail.com>
2018-09-05 17:53:03 +0800

    add: RESTful project

---
+++ 13-RESTful/RESTfulBlogApp/app.js
@@ -0,0 +1,40 @@
+const express =    require('express'),
+      app =        express(),
+      bodyParser = require('body-parser'),
+      mongoose =   require('mongoose')
+
+//APP CONFIG      
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true })
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+})
+const Blog = mongoose.model("Blog", blogSchema)
+
+//RESTFUL ROUTES
+app.get('/', (req, res) => {
+  res.redirect('/blogs')
+})
+
+app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if(err){
+      console.log(err)
+    } else {
+      res.render('index', {blogs})
+    }
+  })
+})
+
+
+
+app.listen(3000, () => {
+  console.log('RESTfulBlogApp server started!')
+})
