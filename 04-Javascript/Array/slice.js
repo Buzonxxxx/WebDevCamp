af26ec4ef79929a102afe15e81e152bfa92f9177
Louis Liao <buzonliao@gmail.com>
2018-10-09 21:57:02 +0800

    complete recursion part

---
+++ 04-Javascript/Array/slice.js
@@ -0,0 +1,8 @@
+const ages = [1, 2, 3, 4, 5, 6]
+// slice 方法會"複製"元素一直到 end 所代表的元素，但並不包括此元素
+// array.slice(start, end(不算))
+console.log(ages.slice(0,1))
+console.log(ages)
+console.log(ages.slice(1))
+console.log(ages.slice(2))
+
--- 04-Javascript/Array/splice.js
+++ 04-Javascript/Array/splice.js
@@ -1,11 +1,7 @@
-const ages = [1, 2, 3, 4, 5, 6]
-// slice 方法會"複製"元素一直到 end 所代表的元素，但並不包括此元素
-// array.slice(start, end(不算))
-console.log(ages.slice(0,1))
-console.log(ages)
-
 // splice: 從 start 位置開始"移除"指定的元素個數，然後插入新元素。刪除的元素會當做新的 Array 物件傳回
 // array.splice(start, deleteCount, item1, ....., itemX)
+const ages = [1, 2, 3, 4, 5, 6]
+
 console.log(ages.splice(0, 1))
 console.log(ages)
 
