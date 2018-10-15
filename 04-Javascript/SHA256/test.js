192e470a45bda072fccc6cfbd052de2d95aced31
Louis Liao <buzonliao@gmail.com>
2018-10-15 13:17:32 +0800

    update sha256

--- 04-Javascript/SHA256/test.js
+++ 04-Javascript/SHA256/test.js
@@ -5,3 +5,12 @@ md.update('The quick brown fox jumps over the lazy dog');
 console.log(md.digest().toHex());
 
 // https://github.com/digitalbazaar/forge
+
+
+const crypto = require('crypto');
+
+const secret = 'abcdefg';
+const hash = crypto.createHmac('sha256', secret)
+                   .update('I love cupcakes')
+                   .digest('hex');
+console.log(hash);
