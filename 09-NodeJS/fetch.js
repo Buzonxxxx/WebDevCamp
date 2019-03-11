68730fe526a5515f0db176cc033bbefca306e8e5
Louis Liao <buzonliao@gmail.com>
2019-03-11 09:12:45 +0800

    update node js

--- 09-NodeJS/fetch.js
+++
@@ -1,6 +0,0 @@
-url = "https://jsonplaceholder.typicode.com/posts12132/"
-
-fetch(url)
-  .then(response => console.log(response))
-  .catch(error => console.log('BAD', error))
-  // .then(data => console.log(data))
