d1b0d20af725bbfb2759722d7bd37d7e3d8c3022
Louis Liao <buzonliao@gmail.com>
2019-03-15 02:44:35 +0800

    update pairSUm

--- 04-Javascript/Algorithm/Patterns/multiple-pointers/pairSum.js
+++ 04-Javascript/Algorithm/Patterns/multiple-pointers/pairSum.js
@@ -4,15 +4,29 @@
 // [1 ,2, 4, 4] sum = 8
 //  i         j
 
-function pairSum(arr){
-  var i = 0, j = arr.length - 1;
-  while(i < j) {
-    let sum = arr[i] + arr[j]
-    if(sum === 8) return [arr[i], arr[j]];
-    else if(sum > 8) j--;
-    else i++;
+// function pairSum(arr){
+//   var i = 0, j = arr.length - 1;
+//   while(i < j) {
+//     let sum = arr[i] + arr[j]
+//     if(sum === 8) return [arr[i], arr[j]];
+//     else if(sum > 8) j--;
+//     else i++;
+//   }
+//   // return [arr[i], arr[j]];
+// }
+
+// console.log(pairSum([1 ,2, 4, 4]))
+
+const twoSum = (nums, target) => {
+  nums = nums.sort()
+  let i = 0, j = nums.length - 1
+  while ( i < j) {
+      let sum = nums[i] + nums[j]
+      if(sum == target) return [i, j]
+      else if (sum > 9) j--
+      else i++
+      j-=1
   }
-  return [arr[i], arr[j]];
 }
 
-console.log(pairSum([1 ,2, 4, 4]))
+console.log(twoSum([3,2,4], 6))
