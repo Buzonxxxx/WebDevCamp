03d46dc4b0a6fe6cfc697543d38f5e925f0c9709
Louis Liao <buzonliao@gmail.com>
2019-03-15 17:35:22 +0800

    add sample code in map

--- 04-Javascript/Algorithm/Patterns/multiple-pointers/pairSum.js
+++ 04-Javascript/Algorithm/Patterns/multiple-pointers/pairSum.js
@@ -4,29 +4,16 @@
 // [1 ,2, 4, 4] sum = 8
 //  i         j
 
-// function pairSum(arr){
-//   var i = 0, j = arr.length - 1;
-//   while(i < j) {
-//     let sum = arr[i] + arr[j]
-//     if(sum === 8) return [arr[i], arr[j]];
-//     else if(sum > 8) j--;
-//     else i++;
-//   }
-//   // return [arr[i], arr[j]];
-// }
-
-// console.log(pairSum([1 ,2, 4, 4]))
-
-const twoSum = (nums, target) => {
-  nums = nums.sort()
-  let i = 0, j = nums.length - 1
-  while ( i < j) {
-      let sum = nums[i] + nums[j]
-      if(sum == target) return [i, j]
-      else if (sum > 9) j--
-      else i++
-      j-=1
+function pairSum(arr){
+  var i = 0, j = arr.length - 1;
+  while(i < j) {
+    let sum = arr[i] + arr[j]
+    if(sum === 8) return [arr[i], arr[j]];
+    else if(sum > 8) j--;
+    else i++;
   }
+  // return [arr[i], arr[j]];
 }
 
-console.log(twoSum([3,2,4], 6))
+console.log(pairSum([1 ,2, 4, 4]))
+
--- 04-Javascript/Data_Structure/Map.js
+++ 04-Javascript/Data_Structure/Map.js
@@ -1,54 +1,69 @@
-/*
-pros use Map rather than obj: 
-1. Anything can be keys
-2. map is iterable
-3. easy to get map size
-4. easy to add/remove value in map
+// /*
+// pros use Map rather than obj: 
+// 1. Anything can be keys
+// 2. map is iterable
+// 3. easy to get map size
+// 4. easy to add/remove value in map
 
-When to use?
-=> you should always use a Map when you need a key-value collection
-https://en.wikipedia.org/wiki/Associative_array
-*/
+// When to use?
+// => you should always use a Map when you need a key-value collection
+// https://en.wikipedia.org/wiki/Associative_array
+// */
 
-const question = new Map()
-question.set('question', 'What is the official name of the latest major Javascript version?')
-question.set(1, 'ES5') 
-question.set(2, 'ES6')
-question.set(3, 'ES2015')
-question.set(4, 'ES7')
-question.set('correct', 3)
-question.set(true, 'Correct answer :D')
-question.set(false, 'Wrong, please try again!')
+// const question = new Map()
+// question.set('question', 'What is the official name of the latest major Javascript version?')
+// question.set(1, 'ES5') 
+// question.set(2, 'ES6')
+// question.set(3, 'ES2015')
+// question.set(4, 'ES7')
+// question.set('correct', 3)
+// question.set(true, 'Correct answer :D')
+// question.set(false, 'Wrong, please try again!')
 
-console.log(question.get('question'))
-console.log(question.size)
-if(question.has(4)) question.delete(4)
-console.log(question.entries())
-console.log(question.keys())
-console.log(question.values())
+// console.log(question.get('question'))
+// console.log(question.size)
+// if(question.has(4)) question.delete(4)
+// console.log(question.entries())
+// console.log(question.keys())
+// console.log(question.values())
 
-question.forEach( (value, key) => console.log(`This is ${key}, and it's set to ${value}`))
+// question.forEach( (value, key) => console.log(`This is ${key}, and it's set to ${value}`))
 
-for (let [key, value] of question.entries()){
-  if (typeof(key) === 'number'){
-    console.log(`Answer ${key}: ${value}`)
+// for (let [key, value] of question.entries()){
+//   if (typeof(key) === 'number'){
+//     console.log(`Answer ${key}: ${value}`)
+//   }
+// }
+
+// const ans = 3
+// console.log(question.get(ans === question.get('correct')))
+
+// question.clear()
+// console.log(question.entries())
+
+// /* 
+// clear()
+// delete(key)
+// entries()
+// get(key)
+// has(key)
+// keys()
+// values()
+// set(key, value)
+// forEach()
+// */
+
+
+var twoSum = function(nums, target) {
+  const map = new Map();
+  for (let i = 0; i<nums.length - 1; i++){
+      if (!map.has(target - nums[i])) { 
+          map.set(nums[i], i)
+          console.log(map)
+         }
+      else {
+        return [map.get(target - nums[i]), i]
+      }
   }
-}
-
-const ans = 3
-console.log(question.get(ans === question.get('correct')))
-
-question.clear()
-console.log(question.entries())
-
-/* 
-clear()
-delete(key)
-entries()
-get(key)
-has(key)
-keys()
-values()
-set(key, value)
-forEach()
-*/
+};
+console.log(twoSum([1,2,6,8], 8))
