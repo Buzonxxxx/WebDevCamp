aed50bde2f8e09a4a2e4af9a9427cc64c4c9eaa3
Louis Liao <buzonliao@gmail.com>
2019-04-17 16:27:48 +0800

    add arrow function example

--- 09-NodeJS/notes-node/json_playground/arrow-function.js
+++ 09-NodeJS/notes-node/json_playground/arrow-function.js
@@ -6,12 +6,31 @@ console.log(square(9))
 
 const user = {
   name: 'Louis',
-  sayHi: () => {
-    console.log(`Hi. I'm ${this.name}`)
-  },
+  guestList: ['Louis', 'Jane', 'Mike'],
   sayHiAlt() {
     console.log(`Hi. I'm ${this.name}`)
+    this.guestList.forEach(guest => {
+      console.log(guest)
+    })
   }
 }
-user.sayHi()
-user.sayHiAlt()
+// user.sayHiAlt()
+
+
+const tasks = {
+  task: [{
+    text: "Grocery shopping",
+    completed: true
+  },{
+    text: "Clean yard",
+    completed: false
+  },{
+    text: "Film course",
+    completed: false
+  }],
+  getTasksToDo() {
+    return this.task.filter(task => task.completed == false )
+  }
+}
+
+console.log(tasks.getTasksToDo())
