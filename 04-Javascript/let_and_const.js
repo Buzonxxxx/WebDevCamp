047947e762b58e4248ca2072a83333077ec454a2
Louis Liao <buzonliao@gmail.com>
2019-05-19 14:23:51 +0800

    remove express part

--- 04-Javascript/let_and_const.js
+++ 04-Javascript/let_and_const.js
@@ -5,14 +5,14 @@ var is function scope
 
 // let => block scope
 let i = 23;
-for (let i = 0; i<5 ; i++){
-  console.log(i)
+for (let i = 0; i < 5; i++) {
+  console.log(i);
 }
-console.log(i)
+console.log(i);
 
 // var function scope
 var j = 23;
-for (var j = 0; j<5 ; j++){
-  console.log(j)
+for (var j = 0; j < 5; j++) {
+  console.log(j);
 }
-console.log(j)
+console.log(j);
