60dd64007da8eef848bb3b7bc6afebde5b998b74
Louis Liao <buzonliao@gmail.com>
2019-09-12 14:26:34 +0800

    add eslint setup

--- 11-API/movie-search-app/app.js
+++ 11-API/movie-search-app/app.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const app = express()
-const request = require('request')
+const express = require('express');
 
-app.set('view engine', 'ejs')
+const app = express();
+const request = require('request');
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('search')
-})
+  res.render('search');
+});
 
 app.get('/results', (req, res) => {
-  const query = req.query.search
+  const query = req.query.search;
   request(`http://www.omdbapi.com/?apikey=thewdb&s=${query}`, (error, response, body) => {
-    if(!error && response.statusCode == 200){
-      const results = JSON.parse(body)
-      res.render('results', {results})
+    if (!error && response.statusCode === 200) {
+      const results = JSON.parse(body);
+      res.render('results', { results });
     }
-  })
-})
+  });
+});
 
 app.listen(3000, () => {
-  console.log('Movie app has started!!!')
-})
+  console.log('Movie app has started!!!');
+});
