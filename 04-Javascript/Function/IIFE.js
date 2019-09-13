a1a977b66fa4247eb6b96e6a7b1fadc7a7e02625
Louis Liao <buzonliao@gmail.com>
2019-09-13 10:15:21 +0800

    update IIFE

--- 04-Javascript/Function/IIFE.js
+++ 04-Javascript/Function/IIFE.js
@@ -1,26 +1,3 @@
-//IIFE
-
-//Original function
-// function game(){
-//   let score = Math.random()*10
-//   if (score >=5){
-//     console.log(score)
-//   }
-// }
-// game()
-
-//IIFE without argument
-// (function () {
-//   let score = Math.random()*10
-//   if (score >= 5){
-//     console.log(score)
-//   }
-// })()
-
-//IIFE with argument
-(function (num) {
-  let score = Math.random()*10
-  if (score >= num){
-    console.log(score)
-  }
-})(5)
+(function () {
+  console.log('hello')
+})()
