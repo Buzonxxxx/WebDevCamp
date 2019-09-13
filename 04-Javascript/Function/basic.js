a413853d1b626c4379c222cb11ff73992cb35c24
Louis Liao <buzonliao@gmail.com>
2019-09-13 14:15:53 +0800

    update xx

--- 04-Javascript/Function/basic.js
+++ 04-Javascript/Function/basic.js
@@ -2,7 +2,8 @@
 const isEven = num => num % 2 === 0 ? true : false
 console.log(isEven(333))
 
-//factorial 練習改用遞迴寫
+// factorial 練習改用遞迴寫
+
 const factorial = num => {
 	sum = 1
 	for (let i = 1; i <= num; i++) {
