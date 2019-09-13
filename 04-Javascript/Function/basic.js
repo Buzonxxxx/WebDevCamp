e4be8faf629e2e24c104d9f62cc59f24a1445378
Louis Liao <buzonliao@gmail.com>
2019-09-13 14:14:16 +0800

    update basic

--- 04-Javascript/Function/basic.js
+++ 04-Javascript/Function/basic.js
@@ -1,13 +1,13 @@
-// is even?
-const isEven = num => num % 2 === 0 ? true : false
-console.log(isEven(333))
+// // is even?
+// const isEven = num => num % 2 === 0 ? true : false
+// console.log(isEven(333))
 
-//factorial 練習改用遞迴寫
-const factorial = num => {
-	sum = 1
-	for (let i = 1; i <= num; i++) {
-		sum *= i;
-	}
-	return sum
-}
-console.log(factorial(10));
+// //factorial 練習改用遞迴寫
+// const factorial = num => {
+// 	sum = 1
+// 	for (let i = 1; i <= num; i++) {
+// 		sum *= i;
+// 	}
+// 	return sum
+// }
+// console.log(factorial(10));
