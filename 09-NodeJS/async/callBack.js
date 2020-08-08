0a4eb314f667ccc45edd60d10a32fe4133bf90fa
Louis Liao <louisliao@Louis-Liao-MBP-C02YV1Q0LVDF.local>
2020-08-08 13:19:26 +0800

    format

--- 09-NodeJS/async/callBack.js
+++ 09-NodeJS/async/callBack.js
@@ -1,19 +1,25 @@
 const getRecipe = () => {
-  setTimeout(() => {
-    const recipeID = [523, 883, 432, 874];
-    console.log(recipeID);
+	setTimeout(() => {
+		const recipeID = [ 523, 883, 432, 874 ];
+		console.log(recipeID);
 
-    setTimeout(id => {
-      const recipe = { title: 'Fresh tomato pasta', publisher: 'Louis'}
-      console.log(`${id}: ${recipe.title}`);
-      
-      setTimeout(publisher => {
-        const recipe2 = { title: 'Italian Pizza', publisher: 'Jason'}  
-        console.log(`title: ${recipe2.title}, Publisher: ${publisher}`);
-      }, 1500, recipe.publisher);
+		setTimeout(
+			(id) => {
+				const recipe = { title: 'Fresh tomato pasta', publisher: 'Louis' };
+				console.log(`${id}: ${recipe.title}`);
 
-    }, 1000, recipeID[2]);
-
-  }, 1500);
-}
+				setTimeout(
+					(publisher) => {
+						const recipe2 = { title: 'Italian Pizza', publisher: 'Jason' };
+						console.log(`title: ${recipe2.title}, Publisher: ${publisher}`);
+					},
+					1500,
+					recipe.publisher
+				);
+			},
+			1000,
+			recipeID[2]
+		);
+	}, 1500);
+};
 getRecipe();
