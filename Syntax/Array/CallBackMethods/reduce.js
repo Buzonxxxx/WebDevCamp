2eae38dd4df4fbe8e63a129a3f649f862fcf5295
Louis Liao <louisliao@Louis-Liao-MBP-C02YV1Q0LVDF.local>
2020-08-12 17:58:32 +0800

    update reduce

--- Syntax/Array/CallBackMethods/reduce.js
+++ Syntax/Array/CallBackMethods/reduce.js
@@ -2,16 +2,21 @@
 
 // 加總
 const numbers = [ 10, 20, 30 ];
-const result = numbers.reduce((sum, number) => sum + number, 0);
-console.log(result); // 60
+const total = numbers.reduce((sum, number) => sum + number, 0);
+console.log(total); // 60
+
+// Finding max value
+const grades = [ 89, 96, 58, 77, 62, 93, 81, 99, 73 ];
+const topScore = grades.reduce((max, grade) => Math.max(grade, max));
+console.log(topScore);
 
 // 取值
 const primaryColors = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' } ];
-const result2 = primaryColors.reduce((previous, primaryColor) => {
+const result = primaryColors.reduce((previous, primaryColor) => {
 	previous.push(primaryColor.color);
 	return previous;
 }, []);
-console.log(result2); // [ 'red', 'blue', 'yellow' ]
+console.log(result); // [ 'red', 'blue', 'yellow' ]
 
 // 陣列扁平化
 const myArr = [ [ 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ] ];
