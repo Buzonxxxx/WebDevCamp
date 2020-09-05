b892502bfd1dbe7aa67dcef5e768a3c01eebd930
Louis Liao <buzonliao@gmail.com>
2020-09-05 09:42:30 +0800

    tweak format

--- DOM/04-Score_Keeper/script.js
+++ DOM/04-Score_Keeper/script.js
@@ -1,51 +1,52 @@
-const p1Button = document.querySelector("#p1")
-const p2Button = document.querySelector("#p2")
-const resetButton = document.querySelector("#reset")
-const p1Display = document.getElementById("p1display")
-const p2Display = document.getElementById("p2display")
-const numInput = document.querySelector("input")
-const winningScoreDisplay = document.querySelector("p span")
-let p1Score = 0
-let p2Score = 0
-let gameOver = false
-let winningScore = 5  
+const p1Button = document.querySelector('#p1');
+const p2Button = document.querySelector('#p2');
+const resetButton = document.querySelector('#reset');
+const p1Display = document.getElementById('p1display');
+const p2Display = document.getElementById('p2display');
+const numInput = document.querySelector('input');
+const winningScoreDisplay = document.querySelector('p span');
+let p1Score = 0;
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
 
-p1Button.addEventListener("click", () => {
-  if(!gameOver){
-  p1Score++
-  if(p1Score === winningScore){
-    p1Display.classList.add("winner")
-    gameOver = true
-   }}
-  p1Display.textContent = p1Score
-})
-p2Button.addEventListener("click", () => {
-  if(!gameOver){
-    p2Score++
-    if(p2Score === winningScore){
-      p2Display.classList.add("winner")
-      gameOver = true
-    }
-  }
-  p2Display.textContent = p2Score
-})
+p1Button.addEventListener('click', () => {
+	if (!gameOver) {
+		p1Score++;
+		if (p1Score === winningScore) {
+			p1Display.classList.add('winner');
+			gameOver = true;
+		}
+	}
+	p1Display.textContent = p1Score;
+});
+p2Button.addEventListener('click', () => {
+	if (!gameOver) {
+		p2Score++;
+		if (p2Score === winningScore) {
+			p2Display.classList.add('winner');
+			gameOver = true;
+		}
+	}
+	p2Display.textContent = p2Score;
+});
 
-resetButton.addEventListener("click", () => {
-  reset()
-})
+resetButton.addEventListener('click', () => {
+	reset();
+});
 
-const reset = () => { 
-p1Display.classList.remove("winner")
-p2Display.classList.remove("winner")
-gameOver = false
-p1Score = 0
-p2Score = 0
-p1Display.textContent = 0
-p2Display.textContent = 0
-}
+const reset = () => {
+	p1Display.classList.remove('winner');
+	p2Display.classList.remove('winner');
+	gameOver = false;
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+};
 
-numInput.addEventListener("change", () => {
-  winningScoreDisplay.textContent = event.target.value
-  winningScore = Number(event.target.value)
-  reset()
-})
+numInput.addEventListener('change', () => {
+	winningScoreDisplay.textContent = event.target.value;
+	winningScore = Number(event.target.value);
+	reset();
+});
--- DOM/05-ToDoList/todo.js
+++ DOM/05-ToDoList/todo.js
@@ -1,14 +1,14 @@
-const input = document.querySelectorAll("input")
-const label = document.querySelectorAll("label")
+const inputs = document.querySelectorAll('input');
+const labels = document.querySelectorAll('label');
 
-for(let i = 0; i<input.length; i++) {
-  input[i].addEventListener("change", () => {
-      label[i].classList.toggle("done")
-  })
-  label[i].addEventListener("mouseover", () => {
-    label[i].classList.add("selected")
-  })
-  label[i].addEventListener("mouseout", () => {
-    label[i].classList.remove("selected")
-  })
+for (let i = 0; i < inputs.length; i++) {
+	inputs[i].addEventListener('change', () => {
+		labels[i].classList.toggle('done');
+	});
+	labels[i].addEventListener('mouseover', () => {
+		labels[i].classList.add('selected');
+	});
+	labels[i].addEventListener('mouseout', () => {
+		labels[i].classList.remove('selected');
+	});
 }
