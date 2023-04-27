fa784fcb69cb6af7fe6375ad74fc8b1fd1b5c785
louis.liao <buzonliao@gmail.com>
2023-04-27 14:55:09 +0800

    verify JSON

--- Algorithm/Questions/verifyJSON.js
+++ Algorithm/Questions/verifyJSON.js
@@ -4,54 +4,75 @@
 
 // console.log(getHelloWorldStr());
 
-// {
-//   "email": "song1234@kimo.com",
-//   "keywords": [
-//     "dailymotion",
-//     "dramaq",
-//     "創業",
-//     "一夜新娘",
-//     "趙薇",
-//     "劉嘉玲",
-//     "肖戰",
-//     "孫藝珍"
-//   ],
-//   "keywords frequency": {
-//     "dailymotion": -3, 
-//     "dramaq": 0,
-//     "一夜新娘": 2,
-//     "趙薇": 29482,
-//     "劉嘉玲": 10,
-//     "肖戰": 1,
-//     "孫藝珍": 4
-//   },
-//   "keywords recency": {
-//     "dailymotion": 5,
-//     "dramaq": 6,
-//     "一夜新娘": 5,
-//     "趙薇": 15,
-//     "劉嘉玲": 15,
-//     "肖戰": 32
-//   },
-//   "Keyword Recency Date": {
-//     "創業": "2020-01-02",
-//     "dailymotion": "2021-02-04",
-//     "dramaq": "2021-02-03",
-//     "一夜新娘": "2021-02-04",
-//     "趙薇": "2021-02-01",
-//     "劉嘉玲": "2021-01-25",
-//     "肖戰": "2021-01-08"
-//   }
+let jsondata = {
+  "email": "song1234@kimo.com",
+  "keywords": [
+    "dailymotion",
+    "dramaq",
+    "創業",
+    "一夜新娘",
+    "趙薇",
+    "劉嘉玲",
+    "肖戰",
+    "孫藝珍"
+  ],
+  "keywords frequency": {
+    "dailymotion": -3, 
+    "dramaq": 0,
+    "一夜新娘": 2,
+    "趙薇": 29482,
+    "劉嘉玲": 10,
+    "肖戰": 1,
+    "孫藝珍": 4
+  },
+  "keywords recency": {
+    "dailymotion": 5,
+    "dramaq": 6,
+    "一夜新娘": 5,
+    "趙薇": 15,
+    "劉嘉玲": 15,
+    "肖戰": 32
+  },
+  "Keyword Recency Date": { // use date obj, now is 2021-02-06
+    "創業": "2020-01-02",
+    "dailymotion": "2021-02-04",
+    "dramaq": "2021-02-03",
+    "一夜新娘": "2021-02-04",
+    "趙薇": "2021-02-01",
+    "劉嘉玲": "2021-01-25",
+    "肖戰": "2021-01-08"
+  }
+}
 
+const isValidJson = (jsonData) => {
+  const obj = jsonData
+  const keywords = obj.keywords
+  const keywordsFrequency = obj["keywords frequency"]
+  const keywordsRecency = obj["keywords recency"]
+  const KeywordRecencyDate = obj["Keyword Recency Date"]
+  for(let key of keywords) {
+    const frequency = keywordsFrequency[key]
+    const recency = keywordsRecency[key]
+    const recencyDate = KeywordRecencyDate[key]
+    const date = new Date(recencyDate)
+    const nowDate = new Date("2021-02-06")
+    const diff = (nowDate - date) / (24*60*60*1000)
 
+    if(frequency < 0) {
+      console.log(`The ${key}'s frequency: ${frequency} < 0`)
+      // return false
+    }
+    if(recency > 12) {
+      console.log(`The ${key}'s recency: ${recency} > 12`)
+      // return false
+    }
+    if (diff >= 6) {
+      console.log(`The ${key}'s date: ${recencyDate} > 6`)
+      // return false
+    }
 
-const validFrequence = (jsonData) => {
-  const obj = JSON.parse(jsonData)
-  const keyword = obj.keywords
-  const keywords_frequency = obj.keywordsFrequency
-  // loop keyword obj and compare with keywords_frequency
-  // iterate keywords_frequency and validate value range in 1~15
-
+    }
   }
 
-}
+
+  console.log(isValidJson(jsondata))
