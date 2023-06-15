60fd045321e1dbb2b6502ca0f11a5208ded50a80
Louis Liao <buzonliao@gmail.com>
2023-06-15 12:16:37 +0800

    update queue

--- Algorithm/Data_Structure/Queue/new_queue.js
+++ Algorithm/Data_Structure/Queue/new_queue.js
@@ -1,8 +1,15 @@
 // --- Directions
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+
 // Implement a 'peek' method in this Queue class.
 // Peek should return the last element (the next
 // one to be returned) from the queue *without*
 // removing it.
+
+// FIFO, First-in-First-out
 
 class Queue {
   constructor() {
@@ -22,4 +29,13 @@ class Queue {
   }
 }
 
-module.exports = Queue;
+const q = new Queue();
+q.add(1)
+q.add(2)
+q.add(3)
+console.log(q)
+
+console.log(q.remove())
+console.log(q)
+
+console.log(q.peek())
